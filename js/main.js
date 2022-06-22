2b1877fee25adbbafdae9d90cfe98f64fa73300b
Prof OKalu <okalu1@outlook.com>
2022-06-21 20:29:18 -0500

    Added JavaScript map-reduce code

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@
 "use strict";
 
 console.log("Hello git demo1");
+const mapReduce = (primeMultiplier, start, ...data) => {
+    return data.map(n => n * primeMultiplier)
+        .reduce((accum, curr) => accum + curr, start);
+};
+console.log(mapReduce(7, 10, 1, 2, 3, 4, 5));
